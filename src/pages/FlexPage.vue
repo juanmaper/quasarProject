c2ac6a8c9ca3f51d1c855610bcf9ece1c5158c61
Juanma <juanmaper30@gmail.com>
2022-07-17 16:10:43 +0200

    Rows and columns in quasr

--- src/pages/FlexPage.vue
+++ src/pages/FlexPage.vue
@@ -1,7 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     
-    <h1>Flex</h1>
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md"> <!-- 12 slots -->
+      <div class="col bg-primary"></div>
+      <div class="col col-5 bg-secondary"></div>
+      <div class="col col-6 bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-center"> 
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end"> 
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between"> 
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
 
   </q-page>
 </template>
@@ -13,3 +39,11 @@ export default defineComponent({
   name: 'FlexPage'
 })
 </script>
+
+<style scoped>
+
+.col, .col-4, .col-6, .col-12 {
+  height: 50px;
+}
+
+</style>
