4e29d9c8629c0705beb58f2623e3ea7f98d75477
Juanma <juanmaper30@gmail.com>
2022-07-17 17:52:31 +0200

    Flexbox

--- src/pages/FlexPage.vue
+++ src/pages/FlexPage.vue
@@ -28,31 +28,89 @@
       <div class="col col-4 bg-secondary"></div>
     </div>
 
-
     <q-separator spaced />
-    
 
     <div class="row q-mb-md justify-between"> 
       <div class="col-12 col-sm-5 col-md-4 bg-primary"></div>
       <div class="col-12 col-sm-5 col-md-4 bg-secondary"></div>
     </div>
 
+    <q-separator spaced />
+
+    <div class="row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-around">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column q-mb-md items-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column q-mb-md items-end bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+    
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box self-start bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box self-end bg-accent"></div>
+    </div>
+    
 
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'FlexPage'
 })
 </script>
 
 <style scoped>
-
 .col, .col-4, .col-6, .col-12 {
   height: 50px;
 }
-
+.square-box {
+  height: 50px;
+  width: 50px;
+}
+.custom-height {
+  height: 300px;
+}
 </style>
